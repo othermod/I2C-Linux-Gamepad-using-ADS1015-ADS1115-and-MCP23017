11f5989bc4abd4177e2d355dacd696669cfdfe3a
othermod <22604540+othermod@users.noreply.github.com>
2023-05-28 22:13:14 -0400

    update setup script
    the script now scans every i2c bus for the two modules, and it reports the result to the user

--- setup.sh
+++ setup.sh
@@ -6,6 +6,54 @@ if [ $(id -u) -ne 0 ]; then
 	exit 1
 fi
 
+# Paths to the files
+file_paths=("gamepad.c" "scan.c" "Makefile" "gamepad.service")
+
+# Check if the files exist
+for file_path in "${file_paths[@]}"; do
+    if [ ! -f "$file_path" ]; then
+        echo "File $file_path not found!"
+        exit 1
+    fi
+done
+
+echo "Compiling the i2c scanner."
+rm scan 2>/dev/null
+gcc -o scan scan.c
+if [ $? -eq 0 ]; then
+    echo "Scanning every i2c bus to find the two modules."
+else
+    echo "Failed to compile the i2c scanner. Make sure you copied the correct scan.c file."
+    exit 1
+fi
+
+./scan
+
+echo ""
+echo "Using the above results as a guide, enter the number of the i2c bus."
+echo "The default is 1, but it depends on your setup."
+
+while true; do
+    # Ask the user to input a value from 0 to 22
+    read input_value
+
+    # Check if the input_value is between 0 and 22
+    if [[ "$input_value" =~ ^[0-9]+$ ]] && [ "$input_value" -ge 0 ] && [ "$input_value" -le 22 ]; then
+        # Check for the existence of the line in gamepad.c
+        if grep -q "#define I2C_BUS \"/dev/i2c-" "gamepad.c"; then
+            # Use sed command to replace the line in gamepad.c
+            sed -i "s|#define I2C_BUS \"/dev/i2c-.*\"|#define I2C_BUS \"/dev/i2c-$input_value\"|g" gamepad.c
+            echo "The file gamepad.c has been updated successfully."
+            break
+        else
+            echo "The line '#define I2C_BUS \"/dev/i2c-\"' does not exist in gamepad.c. Make sure you copied the correct file."
+            break
+        fi
+    else
+        echo "Invalid input. The input should be a number from 0 to 22. Please try again."
+    fi
+done
+
 echo "Enabling I2C"
 
 INTERACTIVE=False
@@ -46,9 +94,18 @@ echo "Enabling gamepad service"
 systemctl enable gamepad
 }
 
-echo "Compiling the joystick driver"
-make clean
-make
+echo "Compiling the gamepad driver"
+rm gamepad 2>/dev/null
+
+# Compile gamepad.c
+gcc -O3 -o gamepad gamepad.c
+if [ $? -eq 0 ]; then
+    echo "Compilation was successful. The file 'gamepad' was created."
+else
+    echo "Failed to compile the gamepad driver. Make sure you copied the correct gamepad.c file."
+    exit 1
+fi
+
 echo "Disabling and removing existing gamepad service"
 systemctl stop gamepad 2>/dev/null
 systemctl disable gamepad 2>/dev/null
@@ -59,5 +116,3 @@ select yn in "Yes" "No"; do
         No ) break;
     esac
 done
-
-echo "You must reboot to finish the installation"
